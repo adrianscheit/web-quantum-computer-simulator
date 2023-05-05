7b0d29d17baa54f6853a7149c3db8eccc86de040
Adrian Scheit <adrianscheit@gmail.com>
2023-05-05 11:28:41 +0200

    Implement operations service

--- src/app/operations.service.spec.ts
+++ src/app/operations.service.spec.ts
@@ -1,39 +1,80 @@
 import { TestBed } from '@angular/core/testing';
 
+import { Operation } from './domain';
 import { OperationsService } from './operations.service';
 
 describe('OperationsService', () => {
+    const opX: Operation = { gn: 'X', qi: [0] };
+    const opY: Operation = { gn: 'Y', qi: [1] };
+    const opZ: Operation = { gn: 'Z', qi: [2] };
     let service: OperationsService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
         service = TestBed.inject(OperationsService);
+        service.set([]);
     });
 
     describe('complement', () => {
         it('no operations', () => {
-            service.operations = [];
             service.complement();
 
             expect(service.operations).toEqual([]);
         });
 
         it('few operations', () => {
-            service.operations = [
-                { gn: 'X', qi: [0] },
-                { gn: 'Y', qi: [1] },
-                { gn: 'CX', qi: [1, 0] },
-            ];
+            service.set([opX, opY, opZ]);
             service.complement();
 
-            expect(service.operations).toEqual([
-                service.operations[0],
-                service.operations[1],
-                service.operations[2],
-                service.operations[2],
-                service.operations[1],
-                service.operations[0],
-            ]);
+            expect(service.operations).toEqual([opX, opY, opZ, opZ, opY, opX]);
+        });
+    });
+
+    describe('add, edit, move, remove', () => {
+        it('add', () => {
+            service.add(0, opX);
+            service.add(1, opY);
+            service.add(0, opY);
+
+            expect(service.operations).toEqual([opY, opX, opY]);
+        });
+
+        it('add fail', () => {
+            expect(() => service.add(1, opX)).toThrow();
+        });
+
+        it('move forward', () => {
+            service.add(0, opX);
+            service.add(1, opY);
+            service.add(2, opZ);
+            service.move(0, 2);
+
+            expect(service.operations).toEqual([opY, opZ, opX]);
+        });
+
+        it('move backward', () => {
+            service.add(0, opX);
+            service.add(1, opY);
+            service.add(2, opZ);
+            service.move(2, 0);
+
+            expect(service.operations).toEqual([opZ, opX, opY]);
+        });
+
+        it('move fail', () => {
+            expect(() => service.move(0, 0)).toThrow();
+        });
+
+        it('remove', () => {
+            service.add(0, opX);
+            service.add(1, opY);
+            service.remove(0);
+
+            expect(service.operations).toEqual([opY]);
+        });
+
+        it('remove fail', () => {
+            expect(() => service.remove(0)).toThrow();
         });
     });
 });
--- src/app/operations.service.ts
+++ src/app/operations.service.ts
@@ -6,13 +6,57 @@ import { Operation } from './domain';
     providedIn: 'root'
 })
 export class OperationsService {
-    operations: Operation[] = [];
-    operationsChange = new Subject<void>();
+    private _operations: Operation[] = [];
+    get operations(): Operation[] {
+        return this._operations;
+    }
+    private _operationsChange = new Subject<void>();
+    readonly operationsChange = this._operationsChange.asObservable();
 
     complement(): void {
         for (let i = this.operations.length - 1; i >= 0; --i) {
             this.operations.push({ gn: this.operations[i].gn, qi: [...this.operations[i].qi] });
         }
-        this.operationsChange.next();
+        this.update();
+    }
+
+    set(operations: Operation[]): void {
+        this._operations = operations;
+        this.update();
+    }
+
+    add(index: number, operation: Operation): void {
+        this._add(index, operation);
+        this.update();
+    }
+
+    move(oldIndex: number, newIndex: number): void {
+        this._add(newIndex, this._remove(oldIndex));
+        this.update();
+    }
+
+    remove(index: number): void {
+        this._remove(index);
+        this.update();
+    }
+
+    update(): void {
+        this._operationsChange.next();
+    }
+
+    private validateIndex(index: number): void {
+        if (index < 0 || index > this.operations.length) {
+            throw new Error(`invalid index ${index} for array of ${this.operations.length} operations`);
+        }
+    }
+
+    private _add(index: number, operation: Operation): void {
+        this.validateIndex(index);
+        this.operations.splice(index, 0, operation);
+    }
+
+    private _remove(index: number): Operation {
+        this.validateIndex(index + 1);
+        return this.operations.splice(index, 1)[0];
     }
 }
